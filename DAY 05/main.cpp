16ffe864d7fd90d3e04e94a1148671ac352c9166
Himasnhu <himanshubhartimax@gmail.com>
2024-06-06 14:36:07 +0530

    DAY 05 : Bitwise Operators, Left/Right shift and Pre/Post increment and decrement

---
+++ DAY 05/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Bitwise Operators
+
+    // int a = 4;
+    // int b = 6;
+
+    // cout << "a & b -> " << (a & b) << endl;
+    // cout << "a | b -> " << (a | b) << endl;
+    // cout << "a ^ b -> " << (a ^ b) << endl;
+    // cout << "~a -> " << (~a) << endl;
+
+    //  Left shift and Right Shift
+
+    // cout << "17 >> 1 : " << (17 >> 1) << endl;
+    // cout << "17 >> 2 : " << (17 >> 2) << endl;
+    // cout << "19 << 1 : " << (19 << 1) << endl;
+    // cout << "21 << 2 : " << (21 << 2) << endl;
+
+    // Pre/Post increment and  Pre/Post decrement
+
+    // int a = 5;
+    // cout << "++a : " << (++a) << endl;
+    // cout << "a : " << (a) << endl;
+    // cout << "a++ : " << (a++) << endl;
+    // cout << "a : " << (a) << endl;
+
+    // int b = 6;
+    // cout << "--b : " << (--b) << endl;
+    // cout << "b : " << (b) << endl;
+    // cout << "b-- : " << (b--) << endl;
+    // cout << "b : " << (b) << endl;
+
+    // HOME WORK QUESTIONS :
+
+    // Question 1.
+
+    // int a, b = 1;
+    // a = 10;
+    // if (++a)
+    // {
+    //     cout << b;
+    // } else {
+    //     cout << ++b;
+    // }
+
+    // Question 2.
+
+    // int a = 1;
+    // int b = 2;
+
+    // if (a-- > 0 && ++b > 2)
+    // {
+    //     cout << "Stage1 - Inside If ";
+    // }
+    // else
+    // {
+    //     cout << "Stage2 - Inside else ";
+    // }
+    // cout << a << " " << b << endl;
+
+    // Question 3.
+
+    // int a = 1;
+    // int b = 2;
+
+    // if (a-- > 0 || ++b > 2)
+    // {
+    //     cout << "Stage1 - Inside If ";
+    // }
+    // else
+    // {
+    //     cout << "Stage2 - Inside else ";
+    // }
+    // cout << a << " " << b << endl;
+
+    // Question 4.
+
+    // int number = 3;
+    // cout << (25 * (++number));
+
+    // Question 5.
+
+    // int a = 1;
+    // int b = a++;
+    // int c = ++a;
+    // cout << b << endl;
+    // cout << c << endl;
+}
